e37238281e58cbd8c7ed671378aa8535c4a24281
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-10-08 17:19:29 +0300

    Add second testcase

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,7 +1,14 @@
 from pages.swag_labs import SwagLabs
 
-def test_check_swag(browser):
+def test_check_icon(browser):
     site_obj = SwagLabs(browser)
     site_obj.visit()
 
     assert site_obj.exist_icon()
+
+def test_check_login(browser):
+    site_obj = SwagLabs(browser)
+    site_obj.visit()
+
+    assert site_obj.find_element('#user-name')
+    
