c0186f4e74ff02e073a357e0076d57c0c6161dc2
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-10-13 15:38:50 +0300

    Add demoqa tests and logic for second subtask

--- tests/test_check_text.py
+++ tests/test_check_text.py
@@ -6,4 +6,16 @@ def test_footer_text(browser):
     site_obj = DemoQa(browser, site_url)
     site_obj.visit()
 
-    assert site_obj.find_element('footer span').text == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+    footer_text = 'footer span'
+    assert site_obj.get_text(footer_text) == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+
+
+def test_check_elements_text(browser):
+    site_obj = DemoQa(browser, site_url)
+    site_obj.visit()
+
+    elements_button = site_obj.find_element('#app > div > div > div.home-body > div > div:nth-child(1)')
+    elements_button.click()
+    text_locator = "#app > div > div > div > div.col-12.mt-4.col-md-6"
+    assert site_obj.get_text(text_locator) == 'Please select an item from left to start practice.'
