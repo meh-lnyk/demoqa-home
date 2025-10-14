bfb5e6521b0c4af3f985aad1a33d505e8fb34763
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-10-14 13:07:25 +0300

    Add logic for second subtask

--- pages/accordian.py
+++ pages/accordian.py
@@ -3,3 +3,7 @@ from pages.base_page import BasePage
 class Accordian(BasePage):
     content_locator = '#section1Content > p'
     heading_locator = '#section1Heading'
+
+    section_2_child_1_locator = '#section2Content > p:nth-child(1)'
+    section_2_child_2_locator = '#section2Content > p:nth-child(2)'
+    section_3_locator = '#section3Content > p'
--- tests/test_visible_hw.py
+++ tests/test_visible_hw.py
@@ -13,3 +13,12 @@ def test_visible_accordion(browser):
     time.sleep(2)
 
     assert not site_obj.find_element(site_obj.content_locator).is_displayed()
+
+
+def test_visible_accordion_default(browser):
+    site_obj = Accordian(browser, site_url)
+    site_obj.visit()
+
+    assert not site_obj.find_element(site_obj.section_2_child_1_locator).is_displayed()
+    assert not site_obj.find_element(site_obj.section_2_child_2_locator).is_displayed()
+    assert not site_obj.find_element(site_obj.section_3_locator).is_displayed()
