27ab3bd5b96284c4176bff55b4842610b531c2a5
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-10-21 14:50:45 +0300

    Add logic for second subtask

--- tests/test_check_alert.py
+++ tests/test_check_alert.py
@@ -1,4 +1,6 @@
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.alerts import Alerts
 
 @pytest.mark.dependency(name='test_check_timer_alert_button')
@@ -7,3 +9,12 @@ def test_check_timer_alert_button(browser):
     site_obj.visit()
 
     assert site_obj.find_elements(site_obj.timer_alert_button_locator)
+
+@pytest.mark.dependency(depends=['test_check_timer_alert_button'])
+def test_alert_showup_time(browser):
+    site_obj = Alerts(browser)
+    site_obj.visit()
+
+    site_obj.find_element(site_obj.timer_alert_button_locator).click()
+    WebDriverWait(browser, 5).until(EC.alert_is_present())
+    assert EC.alert_is_present()
